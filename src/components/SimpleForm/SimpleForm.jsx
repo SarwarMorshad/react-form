08a5c8438ecac5cb3356a1ae20cb653d2a9272b3
SarwarMorshad <sarwarmorshad69@gmail.com>
2023-09-25 22:13:01 +0200

    Start

---
+++ src/components/SimpleForm/SimpleForm.jsx
@@ -0,0 +1,13 @@
+const SimpleForm = () => {
+  return (
+    <div>
+      <form>
+        <input type="text" />
+        <br />
+        <button>submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SimpleForm;
