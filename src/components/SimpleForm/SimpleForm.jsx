52e6cf5b869b47eb1de6c7e678805bc758f67d67
SarwarMorshad <sarwarmorshad69@gmail.com>
2023-09-25 22:30:28 +0200

    Simple Form Practice

--- src/components/SimpleForm/SimpleForm.jsx
+++ src/components/SimpleForm/SimpleForm.jsx
@@ -1,8 +1,15 @@
 const SimpleForm = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target.name.value);
+    console.log("Form Submitted");
+  };
   return (
     <div>
-      <form>
-        <input type="text" />
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="name" />
+        <br />
+        <input type="email" name="email" />
         <br />
         <button>submit</button>
       </form>
